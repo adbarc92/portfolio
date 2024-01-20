73f439434e8c4ba19bd39d008e820190f0ddaf2b
Alex Barclay <adbarclay92@gmail.com>
2024-01-20 11:14:18 -0700

    Adds header configuration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Alex's Home Page" />
+</svelte:head>
+
+<section>
+  <h1>
+    <span class="welcome">
+
+    </span>
+
+    Welcome to your new<br />SvelteKit app
+  </h1>
+
+  <h2>
+    try editing <strong>src/routes/+page.svelte</strong>
+  </h2>
+
+</section>
