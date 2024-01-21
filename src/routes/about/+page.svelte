4f69c22411fb684b990e938bb70777cb89275cb6
Alex Barclay <adbarclay92@gmail.com>
2024-01-20 20:38:07 -0700

    Moves self-description to About page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,5 +6,9 @@
 <div class="text-column">
 	<h1>About</h1>
 
-	<p>I am a philosopher.</p>
+	<p>
+		I am a software engineer and aspiring entrepreneur working in Boulder, CO. I enjoy hiking,
+		writing, reading, and programming. I am currently learning Svelte and improving my Kotlin while
+		studying political philosophy and the finer points of communication and leadership.
+	</p>
 </div>
